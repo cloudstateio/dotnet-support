18a3f3fb9664b3a14c84b1972b046fb41f92c654
Jonathan Nagy <nagytech@users.noreply.github.com>
2019-11-01 16:55:48 +1100

    Fix build

--- examples/ShoppingCart/build.sh
+++ examples/ShoppingCart/build.sh
@@ -1,11 +1,9 @@
-CONFIGURATION=Debug
-FRAMEWORK=netcoreapp2.2
-ARTIFACT_PATH=bin/$CONFIGURATION/$FRAMEWORK/publish
-MAIN_DLL=EventSourced.ShoppingCart.dll
+CONFIGURATION="Debug"
+FRAMEWORK="netcoreapp2.2"
 
 dotnet publish -c $CONFIGURATION -f $FRAMEWORK &&
 docker build \
-    -t test \
-    --build-arg main_dll=$MAIN_DLL \
-    --build-arg artifact_path=$ARTIFACT_PATH \
-    .
+    -t nagytech/cloudstate-csharp/shopping-cart \
+    --build-arg ARTIFACT_PATH="bin/$CONFIGURATION/$FRAMEWORK/publish" \
+    --build-arg MAIN_DLL="EventSourced.ShoppingCart.dll" \
+    --no-cache .
