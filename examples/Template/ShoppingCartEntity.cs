e7c91e7311cf30e6f78aa3f8c228d18140c5d233
Jonathan Nagy <nagytech@users.noreply.github.com>
2019-11-02 18:47:07 +1100

    Bump to .6

--- examples/Template/ShoppingCartEntity.cs
+++ examples/Template/ShoppingCartEntity.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using CloudState.CSharpSupport.Attributes;
 using CloudState.CSharpSupport.Attributes.EventSourced;
 using CloudState.CSharpSupport.Interfaces.EventSourced.Contexts;
 using Com.Example.Shoppingcart.Persistence;
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.Extensions.Logging;
 
 namespace csharp_support
@@ -13,22 +16,103 @@ namespace csharp_support
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class ShoppingCartEntity
     {
-        private string EntityId { get; }
-        private Dictionary<string, LineItem> Cart { get; }
+        String EntityId { get; }
 
-        public ShoppingCartEntity([EntityId]string entityId)
+        Dictionary<String, Com.Example.Shoppingcart.LineItem> Cart { get; }
+
+        public ShoppingCartEntity([EntityId]String entityId)
         {
             EntityId = entityId;
-            Cart = new Dictionary<string, LineItem>();
+            Cart = new Dictionary<string, Com.Example.Shoppingcart.LineItem>();
+        }
+
+        [Snapshot]
+        public Com.Example.Shoppingcart.Persistence.Cart Snapshot()
+        {
+            var cart = new Com.Example.Shoppingcart.Persistence.Cart();
+            cart.Items.AddRange(Cart.Select(x => Convert(x.Value)));
+            return cart;
+        }
+
+        [SnapshotHandler]
+        public void HandleSnapshot(Com.Example.Shoppingcart.Persistence.Cart cart)
+        {
+            Cart.Clear();
+            foreach (Com.Example.Shoppingcart.Persistence.LineItem item in cart.Items)
+            {
+                Cart.Add(item.ProductId, Convert(item));
+            }
+        }
+
+        [EventHandler(typeof(ItemAdded))]
+        public void ItemAdded(Com.Example.Shoppingcart.Persistence.ItemAdded itemAdded)
+        {
+            Cart.TryGetValue(itemAdded.Item.ProductId, out var item);
+            if (item == null)
+            {
+                item = Convert(itemAdded.Item);
+                Cart.Add(item.ProductId, item);
+            }
+            else
+            {
+                item = new Com.Example.Shoppingcart.LineItem(item)
+                {
+                    Quantity = item.Quantity + itemAdded.Item.Quantity
+                };
+                Cart[item.ProductId] = item;
+            }
+        }
+
+        [EventHandler(typeof(ItemRemoved))]
+        public void itemRemoved(Com.Example.Shoppingcart.Persistence.ItemRemoved itemRemoved, IEventBehaviorContext c)
+        {
+            Cart.Remove(itemRemoved.ProductId);
         }
 
         [CommandHandler]
-        public Cart GetCart()
+        public Com.Example.Shoppingcart.Cart GetCart()
         {
-            ;
-            var cart = new Cart();
+            var cart = new Com.Example.Shoppingcart.Cart();
             cart.Items.AddRange(Cart.Values);
             return cart;
         }
+
+        [CommandHandler]
+        public Empty AddItem(Com.Example.Shoppingcart.AddLineItem item, ICommandContext ctx)
+        {
+            if (item.Quantity <= 0)
+            {
+                ctx.Fail("Cannot add negative quantity of to item" + item.ProductId);
+            }
+            ctx.Emit(
+                new Com.Example.Shoppingcart.Persistence.ItemAdded()
+                {
+                    Item = new Com.Example.Shoppingcart.Persistence.LineItem()
+                    {
+                        ProductId = item.ProductId,
+                        Quantity = item.Quantity
+                    }
+                }
+            );
+            return new Empty();
+        }
+
+        Com.Example.Shoppingcart.LineItem Convert(Com.Example.Shoppingcart.Persistence.LineItem item)
+        {
+            var lineItem = new Com.Example.Shoppingcart.LineItem();
+            lineItem.ProductId = item.ProductId;
+            lineItem.Name = item.Name;
+            lineItem.Quantity = item.Quantity;
+            return lineItem;
+        }
+
+        Com.Example.Shoppingcart.Persistence.LineItem Convert(Com.Example.Shoppingcart.LineItem item) =>
+          new Com.Example.Shoppingcart.Persistence.LineItem()
+          {
+              ProductId = item.ProductId,
+              Name = item.Name,
+              Quantity = item.Quantity
+          };
+
     }
 }
--- publish_packages.sh
+++ publish_packages.sh
@@ -4,7 +4,7 @@ dotnet build -c Release
 cp Cloudstate.CSharpSupport.nuspec src/api/bin/Release/netcoreapp2.2/
 pushd src/api/bin/Release/netcoreapp2.2/
 # TODO: Fix this versioning issue...
-VERSION=3.11.0-rc0.5
+VERSION=3.11.0-rc0.6
 nuget pack -Version $VERSION Cloudstate.CSharpSupport.nuspec
 dotnet nuget push \
     -s https://api.nuget.org/v3/index.json \
