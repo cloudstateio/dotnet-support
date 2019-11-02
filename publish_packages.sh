6b0ec389553da0ead3e1a0910dcee51ec9b36da8
Jonathan Nagy <nagytech@users.noreply.github.com>
2019-11-02 18:33:42 +1100

    Updated nuget

--- publish_packages.sh
+++ publish_packages.sh
@@ -1,8 +1,10 @@
 find . -name *.nupkg | xargs rm
 dotnet clean
 dotnet build -c Release
+cp Cloudstate.CSharpSupport.nuspec src/api/bin/Release/netcoreapp2.2/
 pushd src/api/bin/Release/netcoreapp2.2/
-VERSION=3.11.0-rc0.4
+# TODO: Fix this versioning issue...
+VERSION=3.11.0-rc0.5
 nuget pack -Version $VERSION Cloudstate.CSharpSupport.nuspec
 dotnet nuget push \
     -s https://api.nuget.org/v3/index.json \
